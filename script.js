664df50609c7509af45e247f064c73d66ef5ae37
kasp260i <51888040+kasp260i@users.noreply.github.com>
2023-01-04 22:25:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,7 @@ const currentItem = document.getElementById("current-item");
 const leftButton = document.getElementById("left-button");
 const rightButton = document.getElementById("right-button");
 
-
-
 const items = ["Item 1", "Item 2", "Item 3", "Item 4"];
-
-
 
 let currentIndex = 0;
 var billedenummer = 0;
@@ -15,28 +11,26 @@ var tekst = "img/" + billedenummer +".png"
 displayCurrentItem()
 
 function displayCurrentItem() {
+  billedenummer++;
 
-    billedenummer++;
-   
-
-    if(billedenummer>10){
-        billedenummer=1;
-    }
-    tekst = "img/" + billedenummer +".png"
-    // Create an img element
-    const imgElement = document.createElement('img');
-    // Set the src attribute of the img element to the path of the image you want to display
-    imgElement.src = tekst;
-    // Set the width and height of the image (optional)
-    imgElement.width = '300';
-    imgElement.height = '400';
-  
-    // Clear the innerHTML of the currentItem element
-    currentItem.innerHTML = '';
-    // Append the img element to the currentItem element
-    currentItem.appendChild(imgElement);
-    currentItem.style.left = "0px";
+  if(billedenummer>10){
+      billedenummer=1;
   }
+  tekst = "img/" + billedenummer +".png"
+  // Create an img element
+  const imgElement = document.createElement('img');
+  // Set the src attribute of the img element to the path of the image you want to display
+  imgElement.src = tekst;
+  // Set the width and height of the image (optional)
+  imgElement.width = '300';
+  imgElement.height = '400';
+
+  // Clear the innerHTML of the currentItem element
+  currentItem.innerHTML = '';
+  // Append the img element to the currentItem element
+  currentItem.appendChild(imgElement);
+  currentItem.style.left = "0px";
+}
 
 function swipeLeft() {
   currentItem.style.left = "-100%";
